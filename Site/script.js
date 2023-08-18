f7bc4099d935a1abd46fc215a3bc0a2389b914e6
Carlos Jr <carlosrobjr@icloud.com>
2023-08-17 21:33:28 -0300

    Acréscimo de Cálculo
    Foi acrescentado a variável para a autonomia do carro, com o cálculo: (quantidade de gasolina * km/l do carro)

--- Site/script.js
+++ Site/script.js
@@ -1,8 +1,11 @@
 function calcular() {
     var val = window.document.getElementById('val')
     var res1 = window.document.getElementById('res1')
+    var res2 = window.document.getElementById('res2')
     var v = Number(val.value)
     var quantgas = v/5
+    var autcarro = quantgas * 20
     res1.innerHTML = (`Você pode abastecer: ${quantgas}L`)
+    res2.innerHTML = (`Autonomia do veículo: ${autcarro}Km/L`)
 }
 
